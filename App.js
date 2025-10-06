83849871d5de6746e79bc6efa8d95be77e80a660
Christian Kirschberg <ckirschberg@gmail.com>
2025-10-06 15:55:55 +0200

    initial

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+import React from 'react';
 
 export default function App() {
+    const [text, onChangeText] = React.useState('Useless Text');
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Emily og Victor er i gruppe sammen med Elisabeth</Text>
       <StatusBar style="auto" />
+      
+      <Button
+          title="Nej!"
+          onPress={() => Alert.alert('Simple Button pressed')}
+        />
+      <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+        />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  // container svarer til en css klasse
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: 'gray',
   },
 });
